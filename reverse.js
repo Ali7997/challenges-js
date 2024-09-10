5f51e2d7310cad1b68454882130db4a03ae0e332
Ali7997 <146920590+Ali7997@users.noreply.github.com>
2024-09-11 02:02:32 +0330

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,11 @@
+// write your func here
+function reverseString(str) {
+    const strRev =  str.split('').reverse().join('');
+    console.log(strRev);
+}
+
+reverseString("JavaScript");
+reverseString("TypeScript");
+reverseString("React");
+reverseString("Python");
+
